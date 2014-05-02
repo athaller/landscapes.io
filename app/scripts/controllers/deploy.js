4673c2283b9f17377cfbca6e120b9d8472f3e246
Matthew Fincher <mfincher@urthecast.com>
2014-05-02 16:31:31 -0400

    First commit

---
+++ app/scripts/controllers/deploy.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('seahawkApp')
+    .controller('DeployCtrl', function ($scope, $http, $location) {
+
+        $scope.deployment = {type:"Cyber Warfare Test Range", location:1};
+        $scope.errors = {};
+
+        $scope.go = function ( path ) {
+            console.log(path);
+            $location.path( path );
+        };
+
+        $scope.deploy = function(form){
+
+            var json = {};
+
+            $http.get('/api/awsDeploy')
+                .success(function(err, deployResultMsg) {
+                    if(err){
+                        console.log('err: ' + JSON.stringify(err));
+                    }
+                    else {
+                        $scope.deployResultMsg = deployResultMsg;
+                        console.log(deployResultMsg)
+                        $location.path('/?msg=' + JSON.stringify(deployResultMsg));
+                    }
+                })
+        };
+  });
